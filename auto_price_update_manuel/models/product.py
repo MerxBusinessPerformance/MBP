976deb31fd7872d59df576ba7939346ddceca3f9
angel-adriano <angel.adriano@gmail.com>
2022-04-27 11:24:46 -0500

    USD price update

---
+++ auto_price_update_manuel/models/product.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    precio_usd = fields.Float('Venta USD')
+    costo_usd = fields.Float('Costo USD')
+
+    @api.model
+    def price_update(self):
+        for product in self.search([]):
+           if product.costo_usd > 0:
+               product.standard_price = product.costo_usd * (1 / self.env.ref('base.USD').rate)
+       	   if product.precio_usd > 0:
+               product.lst_price = product.precio_usd * (1 / self.env.ref('base.USD').rate)
+       	   	
