c34f4296f40e8e70fcb7ece0654bbd893e23bd61
AngelAdriano <angel.adriano@gmail.com>
2023-02-25 11:51:13 -0600

    Business requirement modules

---
+++ business_requirement_sale_timesheet/models/project_task.py
@@ -0,0 +1,41 @@
+# Copyright 2019 Tecnativa - Victor M.M. Torres
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields, api, _
+
+
+class ProjectTask(models.Model):
+    _inherit = "project.task"
+
+    business_requirement_deliverable_id = fields.Many2one(
+        comodel_name="business.requirement.deliverable",
+        related="sale_line_id.business_requirement_deliverable_id",
+        readonly=True,
+        string="Deliverable",
+    )
+    business_requirement_id = fields.Many2one(
+        "business.requirement", string="Business requirement"
+    )
+
+    def action_view_deliverable(self):
+        action = self.env.ref(
+            "business_requirement_deliverable.action_deliverable_lines"
+        ).read()[0]
+        action.update(
+            {
+                "view_mode": "form",
+                "views": [],
+                "view_id": False,
+                "res_id": self.business_requirement_deliverable_id.id,
+            }
+        )
+        return action
+
+
+class BusinessRequirementExtend(models.Model):
+    _inherit = "business.requirement"
+
+    task_ids = fields.One2many('project.task', 'business_requirement_id', string='Tasks', )
+    project_id = fields.Many2one(
+        comodel_name='project.project',
+        string='Project')
